e053616f0b4c3048b4ca1e5252ecb48273b8abde
Andrey Polugodin <a.polugodin@escape-team.tech>
2023-03-15 20:58:13 +0400

    fix base dir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
+  base: 'm3u8loader',
+  build: {
+    outDir: 'm3u8loader',
+  },
   server: {
     port: 3000,
   },
